1b8cd50523a085e3296b2ea440d054f37ab1d2a3
som <someshbasak2016@gmail.com>
2023-08-16 12:59:47 +0530

    Day-24: Add lines.

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,0,254)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(x1=10,y1=21, x2=200,y2=21)
+
+    for y in range(20,298,10):
+        pdf.line(x1=10,y1=y, x2=200,y2=y)
 
     #Set the footer
-    pdf.ln(262)
+    pdf.ln(266)
     pdf.set_font(family="Times", style="B", size=8)
     pdf.set_text_color(100, 20, 200)
     pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
@@ -23,9 +25,12 @@ for index, row in df.iterrows():
     for i in range(row["Pages"] - 1 ):
         pdf.add_page()
         # Set the footer
-        pdf.ln(272)
+        pdf.ln(278)
         pdf.set_font(family="Times", style="B", size=8)
         pdf.set_text_color(100, 20, 200)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+        for y in range(20, 298, 10):
+            pdf.line(x1=10, y1=y, x2=200, y2=y)
 
 pdf.output("output.pdf")
